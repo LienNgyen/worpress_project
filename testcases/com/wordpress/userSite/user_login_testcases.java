dcd91a61f12959d82b7453a25c072aa16b048656
Vinhphamtien <vinh.phamtien1805@gmail.com>
2020-10-31 21:21:18 +0700

    test case commit on Oct 31

--- testcases/com/wordpress/userSite/user_login_testcases.java
+++ testcases/com/wordpress/userSite/user_login_testcases.java
@@ -1,7 +1,30 @@
 package com.wordpress.userSite;
 
-public class user_login_testcases {
+import org.testng.annotations.Test;
+
+import commons.AbstractTest;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+
+public class user_login_testcases extends AbstractTest {
+	WebDriver driver;
 	
-	abcsss
+	@Parameters({"browser","url"})
+
+	@BeforeClass
+	public void beforeClass(String browser, String url) {
+		driver = getBrowserDriver(browser, url);
+	}
+
+	@Test
+	public void f() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
 
 }
